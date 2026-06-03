6fad73ba868750757da74d3329a922c6f3883f06
lokesh gm <reddylokesh102004@gmail.com>
2026-06-03 06:56:01 +0000

    Updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,15 @@
+provider "azurerm" {
+  features {}
+}
 
+resource "azurerm_resource_group" "rg" {
+  name     = "healthcare-rg"
+  location = "Central India"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "healthcareacr123"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+}
