6e2b073bf9d88185b590ed9021bc610e96548b30
lokesh <lokilokeshreddy3@gmail.com>
2026-06-03 13:20:01 +0530

    updated terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.6"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 3.0"
     }
   }
 }
