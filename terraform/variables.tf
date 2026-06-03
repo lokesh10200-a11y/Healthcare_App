658fdc93943059e780a0468f4ad0a9b564fbce4b
lokesh gm <reddylokesh102004@gmail.com>
2026-06-03 07:03:33 +0000

    Updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
+variable "resource_group_name" {
+  default = "healthcare_app_project1"
+}
 
+variable "location" {
+  default = "Central India"
+}
+
+variable "acr_name" {
+  default = "healthcareappproject1rg"
+}
+
+variable "aks_name" {
+  default = "healthcareappproject1-aks"
+}
