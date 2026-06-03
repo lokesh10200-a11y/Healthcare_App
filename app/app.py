a2fd1a0efdff70f63e2c7a73bc69cfd70cfea88d
lokesh gm <reddylokesh102004@gmail.com>
2026-06-03 06:53:06 +0000

    Updated app.py

--- app/app.py
+++ app/app.py
@@ -2,16 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return "Healthcare App"
-
-
-@app.route("/health")
-def health():
-    return "Healthy"
-
+    return "Healthcare Application Running"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
