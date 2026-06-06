72cdc380cb912ae6a26e9a2e18a0310f25b1e876
lokesh <lokilokeshreddy3@gmail.com>
2026-06-06 10:23:40 +0530

    Initial working Flask app with pytest

--- app/app.py
+++ app/app.py
@@ -1,17 +1,7 @@
 from flask import Flask
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
 
-
-@app.route("/")
+@flask_app.route("/")
 def home():
-    return "Healthcare App Running"
-
-
-@app.route("/health")
-def health():
-    return {"status": "healthy"}
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return "Hello"
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,6 @@
-from app import app
+from app.app import flask_app
 
 def test_home():
-    client = app.test_client()
-    response = client.get('/')
-
+    client = flask_app.test_client()
+    response = client.get("/")
     assert response.status_code == 200
